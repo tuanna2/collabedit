f1555a25d3c7719067c60860108e8d172c5281f1
Nguyễn Anh Tuấn <tuanna2@ows.com.vn>
2018-12-04 15:35:08 +0700

    change host  databases

--- ShareText/models/connect.js
+++ ShareText/models/connect.js
@@ -1,10 +1,11 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : 'sql12.freemysqlhosting.net',
-      user : 'sql12265721',
-      password : 'i6EFIxJfgc',
-      database : 'sql12265721'
+      host : '125.212.227.42',
+      user : 'root',
+      password : 'toor',
+      database : 'codecungtroc',
+      port:3336
     }
   });
 
