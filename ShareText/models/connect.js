bf9c36ce82d14da3f069dfde25d16b18ff82ed7b
tuanna2 <tuanna2@ows.com.vn@gmail.com>
2019-09-26 15:33:40 +0700

    update db server

--- ShareText/models/connect.js
+++ ShareText/models/connect.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-      host : '125.212.227.42',
-      user : 'root',
-      password : 'toor',
-      database : 'codecungtroc',
-      port:3336
+      host : 'us-cdbr-iron-east-02.cleardb.net',
+      user : 'bafe70826c7a37',
+      password : 'f59a1590',
+      database : 'heroku_4f019047f164d80',
+      acquireConnectionTimeout: 10000
     }
   });
 
