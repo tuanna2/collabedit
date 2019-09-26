16a282fd93ee7a8d9b93b31bdbe1b9375475b771
tuanna2 <tuanna2@ows.com.vn@gmail.com>
2019-09-26 15:47:19 +0700

    FIX SOME BUG

--- ShareText/server.js
+++ ShareText/server.js
@@ -5,7 +5,7 @@ const async = require("async");
 const routes = require('./routes/routes');
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
-http.listen(3000);
+http.listen(process.env.PORT || 3000);
 const socket = require('./routes/socket');
 const sslRedirect = require('heroku-ssl-redirect');
 app.use(sslRedirect());
